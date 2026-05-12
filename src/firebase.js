caf365bc6c11a7ed5707b1883a021435f2db390f
Lucia Ferraro <luciaferraro@MacBook-Air-di-Lucia.local>
2026-05-12 16:28:21 +0200

    modifica

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcB9uCNq944JG9rv2e1DTOiffXL-fF4RU",
+  authDomain: "mymovie-app-f55c2.firebaseapp.com",
+  projectId: "mymovie-app-f55c2",
+  storageBucket: "mymovie-app-f55c2.firebasestorage.app",
+  messagingSenderId: "343301624790",
+  appId: "1:343301624790:web:4cf52b63fda44b21830a77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
