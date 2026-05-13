07cbb45e28412ad941fcbb9b6a26ad5548e4d94b
Elisabetta Rovere <elisabettarovere@Mac.wind3.hub>
2026-05-13 17:55:02 +0200

    modifica

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs, getFirestore, query, where } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,8 +13,57 @@ const firebaseConfig = {
   messagingSenderId: "343301624790",
   appId: "1:343301624790:web:4cf52b63fda44b21830a77"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export const db = getFirestore(app)
+export default {
+  db,
+  isAuthenticated() {
+    // controllo se utente salveto in localStorage (nel browser), restituisce true se c'è, false altrimenti
+    return !!localStorage.getItem("login");
+  },
+
+  // salvare ed eliminare utente dal localStorage (nel browser)
+  getUser() {
+    return localStorage.getItem("login");
+  },
+  setUser(username) {
+    localStorage.setItem("login", username);
+  },
+  logout() {
+    localStorage.removeItem("login");
+  },
+
+  // cerca utente su Firebase nella collection 'utenti', controlla la pw e restituisce l'username
+  async verificaLogin(username, password) {
+    const docRef = await getDoc(doc(db, "utenti", username));
+    if (!docRef.exists()) throw new Error("Username non trovato");
+    if (docRef.data().password !== password) throw new Error("Password errata");
+    return username;
+  },
+
+  // salva nuova recensione su Firebase nella collection 'recensioni'
+  async addRecensione(movieId, movieTitle, testo, voto) {
+    const username = this.getUser();
+    await addDoc(collection(db, "recensioni"), {
+      movieId: String(movieId),
+      movieTitle,
+      username,
+      testo,
+      voto: Number(voto),
+      data: new Date().toISOString()
+    });
+  },
+
+  // cerca recensioni su Firebase nella collection 'recensioni' per un certo movieId
+  // restituisce array di recensioni ordinate per data (dalla più recente alla più vecchia)
+  async getRecensioni(movieId) {
+    const q = query(
+      collection(db, "recensioni"),
+      where("movieId", "==", String(movieId)),
+      orderBy("data", "desc")
+    );
+    const snapshot = await getDocs(q);
+    return snapshot.docs.map(d => ({ id: d.id, ...d.data() }));
+  }
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// importare stili di vuetify, tutti i componenti, le direttive e le icone M<di
+import 'vuetify/styles' 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+}) /* per integrare componenti e direttive di vuetify */
+
+// collega vuetify all'app vue
 const app = createApp(App)
 
 app.use(router)
